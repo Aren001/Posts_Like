761617123ec97f6ecb578c0baf21d3612fd1f9b6
Aren <vardannyan99664470@mail.ru>
2021-01-24 19:33:08 +0400

    first commi

---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/posts', function () {
+//     return view('posts.index');
+// });
+
+
+Route::get('/', function () {
+    return view('home');
+})->name('home');
+
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard')->middleware('auth'); //chi toxum gna dashboard qani grancvac chi
+
+
+//Register
+
+Route::get('/register',[RegisterController::class,'index'])->name('register');
+Route::post('/register', [RegisterController::class, 'store']);
+
+//Login
+
+
+Route::get('/login',[LoginController::class,'index'])->name('login');
+Route::post('/login', [LoginController::class, 'store']);
+
+//LogOut
+Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
